1b4278be66755ced3427b725f280d6228081a02f
Arnie <u22203827@utp.edu.pe>
2024-10-06 15:48:27 -0500

    Inicializando pyro4

--- app.py
+++ app.py
@@ -3,19 +3,28 @@ import Pyro4
 
 app = Flask(__name__)
 
+# URL del servidor de nombres en Render
+NAMESERVER_URL = "PYRONAME:example.factorial@pyro4-9vfb.onrender.com:9090"
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/factorial', methods=['POST'])
 def calcular_factorial():
     numero = int(request.form['numero'])
-    factorial_server = Pyro4.Proxy("PYRONAME:example.factorial")
+
+    # Crear el proxy del servidor de factorial
+    factorial_server = Pyro4.Proxy(NAMESERVER_URL)  # Usamos la URL del servidor de nombres
+
     try:
         resultado = factorial_server.factorial(numero)
         return render_template('index.html', resultado=f"El factorial de {numero} es: {resultado}")
     except Exception as e:
         return render_template('index.html', resultado=f"Error al calcular el factorial: {e}")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
